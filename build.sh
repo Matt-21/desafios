b3e786ddc14470eba92ee19abcb67b4e51c36ba4
Matheus Lopes Novais <m.lopes.1795@gmail.com>
2023-03-10 17:58:31 -0300

    Correções na build shell e insomnia collection

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 composer install
 cp .env.example .env
 php artisan key:generate
-sudo chown $USER -R laravel-docker/storage
+sudo chown $USER -R storage
 docker compose up -d --build
 docker exec -it laravel-docker-laravel-app-1 bash
 cd /usr/share/nginx
